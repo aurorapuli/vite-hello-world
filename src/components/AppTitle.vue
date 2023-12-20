5f56e86075a1e589aa7efe1a1a7ca4fb917c0308
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2023-12-20 16:13:16 +0100

    config: img

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,13 +1,30 @@
 <script>
+export default {
+    data() {
+        return {
+            imageName: "vue.svg"
+        }
+    },
+    methods: {
+        getImagePath: function (imgPath) {
+            return new URL(imgPath, import.meta.url).href;
+        }
+    }
+};
 
 </script>
 
 <template>
     <h1 class="read-the-docs">La mia prima app con Vite!</h1>
+    <img class="img" :src="getImagePath(`../assets/${imageName}`)" alt="vue">
 </template>
 
 <style scoped>
 .read-the-docs {
     color: #888;
+}
+
+.img {
+    width: 100px;
 }
 </style>
